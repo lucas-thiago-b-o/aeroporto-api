b84ce18a678a0cb2e2c2d5b338da17254a3a18ce
lucas-thiago-b-o <lucas-thiago-b-o@proton.me>
2024-01-19 15:27:48 -0300

    Commit Inicial

---
+++ src/main/java/br/com/companhia/aeroporto/AeroportoApplication.java
@@ -0,0 +1,13 @@
+package br.com.companhia.aeroporto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AeroportoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AeroportoApplication.class, args);
+	}
+
+}
