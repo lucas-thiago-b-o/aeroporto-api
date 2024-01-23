ab125576432630022170809f8ddeabfa3647163e
Lucas Thiago <lucas-thiago-b-o@proton.me>
2024-01-22 21:11:29 -0300

    refatoração de código

--- src/main/java/br/com/companhia/aeroporto/config/DatabaseInstanceInitializer.java
+++ src/main/java/br/com/companhia/aeroporto/config/DatabaseInstanceInitializer.java
@@ -6,9 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Component
 public class DatabaseInstanceInitializer {
+
+    public static final Random random = new Random();
 
     @Autowired
     AeroportoRepository aeroportoRepository;
@@ -38,85 +43,145 @@ public class DatabaseInstanceInitializer {
     private VooRepository vooRepository;
 
     public void instanciaDadosNaBase() {
-        Cidade salvador = new Cidade();
-        Cidade portoSeguro = new Cidade();
-        Cidade riodejaneiro = new Cidade();
-        Cidade cruzeiroDoSul = new Cidade();
+        ufRepository.saveAll(List.of(getUfBahia(), getUfRioDeJaneiro(), getUfAcre()));
+
+        cidadeRepository.saveAll(List.of(getCidadeSalvador(), getCidadeRioDeJaneiro(), getCidadePortoSeguro(), getCidadeCruzeiroDoSul()));
+
+        aeroportoRepository.saveAll(List.of(getAeroportoSalvador(), getAeroportoRioDeJaneiroSantosDumont(), getAeroportoRioDeJaneiroGaleao(),
+                                            getAeroportoPortoSeguro(), getAeroportoCruzeiroDoSul()));
+
+        bagagemRepository.save(getBagagem(false));
+
+        passageiroRepository.save(getPassageiro());
+
+        passagemRepository.save(getPassagem());
+    }
+
+    public Uf getUfBahia() {
         Uf bahia = new Uf();
-        Uf rj = new Uf();
-        Uf acre = new Uf();
-        Aeroporto aeroportoSalvador = new Aeroporto();
-        Aeroporto aeroportoCruzeiroDoSul = new Aeroporto();
-        Aeroporto aeroportoPortoSeguro = new Aeroporto();
-        Aeroporto aeroportoRioDeJaneiroSantosDumont = new Aeroporto();
-        Aeroporto aeroportoRioDeJaneiroGaleao = new Aeroporto();
-        Voo vooSalvador1 = new Voo();
-        Passageiro passageiro1 = new Passageiro();
-        Classe classe11 = new Classe();
-        Bagagem bagagem1 = new Bagagem();
-        Assento assento11 = new Assento();
-        Passagem passagem = new Passagem();
 
         bahia.setId(1L);
         bahia.setNome("Bahia");
 
-        rj.setId(2L);
-        rj.setNome("Rio de Janeiro");
+        return bahia;
+    }
+
+    public Uf getUfAcre() {
+        Uf acre = new Uf();
 
         acre.setId(3L);
         acre.setNome("Acre");
 
+        return acre;
+    }
+
+    public Uf getUfRioDeJaneiro() {
+        Uf rj = new Uf();
+
+        rj.setId(2L);
+        rj.setNome("Rio de Janeiro");
+
+        return rj;
+    }
+
+    public Cidade getCidadeSalvador() {
+        Cidade salvador = new Cidade();
+
         salvador.setId(1L);
         salvador.setNome("Salvador");
-        salvador.setUf(bahia);
+        salvador.setUf(getUfBahia());
 
-        riodejaneiro.setId(2L);
-        riodejaneiro.setNome("Rio de Janeiro");
-        riodejaneiro.setUf(rj);
+        return salvador;
+    }
+
+    public Cidade getCidadePortoSeguro() {
+        Cidade portoSeguro = new Cidade();
 
         portoSeguro.setId(3L);
         portoSeguro.setNome("Porto Seguro");
-        portoSeguro.setUf(bahia);
+        portoSeguro.setUf(getUfBahia());
+
+        return portoSeguro;
+    }
+
+    public Cidade getCidadeRioDeJaneiro() {
+        Cidade riodejaneiro = new Cidade();
+
+        riodejaneiro.setId(2L);
+        riodejaneiro.setNome("Rio de Janeiro");
+        riodejaneiro.setUf(getUfRioDeJaneiro());
+
+        return riodejaneiro;
+    }
+
+    public Cidade getCidadeCruzeiroDoSul() {
+        Cidade cruzeiroDoSul = new Cidade();
 
         cruzeiroDoSul.setId(4L);
         cruzeiroDoSul.setNome("Cruzeiro do Sul");
-        cruzeiroDoSul.setUf(acre);
+        cruzeiroDoSul.setUf(getUfAcre());
+
+        return cruzeiroDoSul;
+    }
+
+    public Aeroporto getAeroportoSalvador() {
+        Aeroporto aeroportoSalvador = new Aeroporto();
 
         aeroportoSalvador.setId(1L);
-        aeroportoSalvador.setCidade(salvador);
+        aeroportoSalvador.setCidade(getCidadeSalvador());
         aeroportoSalvador.setNome("Aeroporto Internacional Dep. Luís Eduardo Magalhães");
         aeroportoSalvador.setCodigoAeroportuario("SSA");
 
-        aeroportoRioDeJaneiroSantosDumont.setId(2L);
-        aeroportoRioDeJaneiroSantosDumont.setCidade(riodejaneiro);
-        aeroportoRioDeJaneiroSantosDumont.setNome("Aeroporto do Rio de Janeiro-Santos Dumont");
-        aeroportoRioDeJaneiroSantosDumont.setCodigoAeroportuario("SDU");
+        return aeroportoSalvador;
+    }
 
-        aeroportoRioDeJaneiroGaleao.setId(3L);
-        aeroportoRioDeJaneiroGaleao.setCidade(riodejaneiro);
-        aeroportoRioDeJaneiroGaleao.setNome("Aeroporto Internacional do Rio de Janeiro-Galeão");
-        aeroportoRioDeJaneiroGaleao.setCodigoAeroportuario("GIG");
+    public Aeroporto getAeroportoPortoSeguro() {
+        Aeroporto aeroportoPortoSeguro = new Aeroporto();
 
         aeroportoPortoSeguro.setId(4L);
-        aeroportoPortoSeguro.setCidade(portoSeguro);
+        aeroportoPortoSeguro.setCidade(getCidadePortoSeguro());
         aeroportoPortoSeguro.setNome("Aeroporto de Porto Seguro");
         aeroportoPortoSeguro.setCodigoAeroportuario("BPS");
 
+        return aeroportoPortoSeguro;
+    }
+
+    public Aeroporto getAeroportoCruzeiroDoSul() {
+        Aeroporto aeroportoCruzeiroDoSul = new Aeroporto();
+
         aeroportoCruzeiroDoSul.setId(5L);
-        aeroportoCruzeiroDoSul.setCidade(cruzeiroDoSul);
+        aeroportoCruzeiroDoSul.setCidade(getCidadeCruzeiroDoSul());
         aeroportoCruzeiroDoSul.setNome("Aeroporto Internacional de Cruzeiro do Sul");
         aeroportoCruzeiroDoSul.setCodigoAeroportuario("CZS");
 
-        assento11.setId(1L);
-        assento11.setNome("F5");
+        return aeroportoCruzeiroDoSul;
+    }
 
-        classe11.setId(1L);
-        classe11.setNome("Primeira Classe");
-        classe11.setAssento(assento11);
+    public Aeroporto getAeroportoRioDeJaneiroSantosDumont() {
+        Aeroporto aeroportoRioDeJaneiroSantosDumont = new Aeroporto();
 
-        bagagem1.setId(1L);
-        bagagem1.setNumeroIdentificacao(12345L);
-        bagagem1.setIsDespachada(false);
+        aeroportoRioDeJaneiroSantosDumont.setId(2L);
+        aeroportoRioDeJaneiroSantosDumont.setCidade(getCidadeRioDeJaneiro());
+        aeroportoRioDeJaneiroSantosDumont.setNome("Aeroporto do Rio de Janeiro-Santos Dumont");
+        aeroportoRioDeJaneiroSantosDumont.setCodigoAeroportuario("SDU");
+
+        return aeroportoRioDeJaneiroSantosDumont;
+    }
+
+    public Aeroporto getAeroportoRioDeJaneiroGaleao() {
+        Aeroporto aeroportoRioDeJaneiroGaleao = new Aeroporto();
+
+
+        aeroportoRioDeJaneiroGaleao.setId(3L);
+        aeroportoRioDeJaneiroGaleao.setCidade(getCidadeRioDeJaneiro());
+        aeroportoRioDeJaneiroGaleao.setNome("Aeroporto Internacional do Rio de Janeiro-Galeão");
+        aeroportoRioDeJaneiroGaleao.setCodigoAeroportuario("GIG");
+
+        return aeroportoRioDeJaneiroGaleao;
+    }
+
+    public Passageiro getPassageiro() {
+        Passageiro passageiro1 = new Passageiro();
 
         passageiro1.setId(1L);
         passageiro1.setDataNascimento(LocalDateTime.now());
@@ -126,43 +191,109 @@ public class DatabaseInstanceInitializer {
         passageiro1.setPassaporte(12345L);
         passageiro1.setContatoEmergencia(21912345678L);
         passageiro1.setTelefone(21969487457L);
-        passageiro1.setBagagem(bagagem1);
+        passageiro1.setBagagem(getBagagem(false));
 
-        vooSalvador1.setId(1L);
-        vooSalvador1.setAeroportoOrigem(aeroportoSalvador);
-        vooSalvador1.setAeroportoDestino(aeroportoRioDeJaneiroSantosDumont);
-        vooSalvador1.setDataHoraChegada(LocalDateTime.now());
-        vooSalvador1.setDataHoraMarcado(LocalDateTime.now());
-        vooSalvador1.setDataHoraPartida(LocalDateTime.now());
-        vooSalvador1.setDataHoraPrevisao(LocalDateTime.now());
-        vooSalvador1.setNome("Voo 1");
-        vooSalvador1.setClasse(classe11);
-        vooSalvador1.setPassageiro(passageiro1);
+        return passageiro1;
+    }
+
+    public Bagagem getBagagem(Boolean isDespachado) {
+        Bagagem bagagem1 = new Bagagem();
+
+        bagagem1.setId(1L);
+        bagagem1.setNumeroIdentificacao(12345L);
+        bagagem1.setIsDespachada(isDespachado);
+
+        return bagagem1;
+    }
+
+    public Passagem getPassagem() {
+        Passagem passagem = new Passagem();
 
         passagem.setId(1L);
-        passagem.setVoo(vooSalvador1);
+        passagem.setVoo(getVoo());
         passagem.setDataHoraVoo(LocalDateTime.now());
         passagem.setNumeroIdentificacao(12345L);
         passagem.setPortaoEmbarque("Portao");
-        passagem.setValor(12345L);
+        passagem.setValor(geraValorVooEpassagem());
 
-        ufRepository.save(bahia);
-        ufRepository.save(rj);
-        ufRepository.save(acre);
-        cidadeRepository.save(cruzeiroDoSul);
-        cidadeRepository.save(salvador);
-        cidadeRepository.save(riodejaneiro);
-        cidadeRepository.save(portoSeguro);
-        aeroportoRepository.save(aeroportoSalvador);
-        aeroportoRepository.save(aeroportoPortoSeguro);
-        aeroportoRepository.save(aeroportoCruzeiroDoSul);
-        aeroportoRepository.save(aeroportoRioDeJaneiroSantosDumont);
-        aeroportoRepository.save(aeroportoRioDeJaneiroGaleao);
-        assentoRepository.save(assento11);
-        classeRepository.save(classe11);
-        bagagemRepository.save(bagagem1);
-        passageiroRepository.save(passageiro1);
-        vooRepository.save(vooSalvador1);
-        passagemRepository.save(passagem);
+        return passagem;
+    }
+
+    public Voo getVoo() {
+        Voo voo = new Voo();
+        AtomicLong atomicLong = new AtomicLong();
+        atomicLong.set(0);
+
+        for (char letra = 'A'; letra <= 'Z'; letra++) {
+            for (int i = 1; i <= 26; i++) {
+                long id = atomicLong.incrementAndGet();
+
+                Assento assento = saveAndGetAssento(id, letra, i);
+                Classe classe = saveAndGetClasse(id, i, assento);
+                voo = mountVoo(id, getAeroportoSalvador(), getAeroportoRioDeJaneiroSantosDumont(),
+                                   getAeroportoPortoSeguro(), getAeroportoCruzeiroDoSul(),
+                               classe);
+
+                vooRepository.save(voo);
+            }
+        }
+
+        return voo;
+    }
+
+    private Assento saveAndGetAssento(long id, char letra, int i) {
+        Assento assento = new Assento();
+        assento.setId(id);
+        assento.setNome(String.valueOf(letra) + Integer.toUnsignedLong(i));
+        assentoRepository.save(assento);
+        return assento;
+    }
+
+    private Classe saveAndGetClasse(long id, int i, Assento assento) {
+        Classe classe = new Classe();
+        classe.setId(id);
+        classe.setAssento(assento);
+        classe.setNome(i <= 11 ? "Primeira Classe" : "Classe Econômica");
+        classeRepository.save(classe);
+        return classe;
+    }
+
+    private Voo mountVoo(long id, Aeroporto aeroportoSalvador, Aeroporto aeroportoRioDeJaneiroSantosDumont,
+                                  Aeroporto aeroportoPortoSeguro, Aeroporto aeroportoCruzeiroDoSul,
+                         Classe classe) {
+        Voo voo = new Voo();
+        voo.setId(id);
+
+        if (id <= 169) {
+            voo.setAeroportoOrigem(aeroportoSalvador);
+            voo.setAeroportoDestino(aeroportoRioDeJaneiroSantosDumont);
+        } else if (id >= 338 && id < 507) {
+            voo.setAeroportoOrigem(aeroportoPortoSeguro);
+            voo.setAeroportoDestino(aeroportoRioDeJaneiroSantosDumont);
+        } else if (id >= 507 && id < 591) {
+            voo.setAeroportoOrigem(aeroportoCruzeiroDoSul);
+            voo.setAeroportoDestino(aeroportoRioDeJaneiroSantosDumont);
+        } else {
+            voo.setAeroportoOrigem(aeroportoRioDeJaneiroSantosDumont);
+            voo.setAeroportoDestino(aeroportoSalvador);
+        }
+
+        voo.setDataHoraChegada(LocalDateTime.now());
+        voo.setDataHoraMarcado(LocalDateTime.now().plusHours(24));
+        voo.setDataHoraPartida(LocalDateTime.now());
+        voo.setDataHoraPrevisao(LocalDateTime.now());
+        voo.setNome("Voo " + id);
+        voo.setClasse(classe);
+        voo.setStatus("Programado");
+        voo.setValor(geraValorVooEpassagem());
+        voo.setPassageiro(id == 1 ? getPassageiro() : null);
+
+        return voo;
+    }
+
+    public Long geraValorVooEpassagem() {
+        int valorAleatorio = random.nextInt(39001) + 1000;
+
+        return Integer.toUnsignedLong(valorAleatorio);
     }
 }
