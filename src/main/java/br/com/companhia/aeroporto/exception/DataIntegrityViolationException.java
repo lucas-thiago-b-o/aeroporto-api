c2fb9aee2f7ca14c9dfa2f4df5aab796817713ad
Lucas Thiago <lucas-thiago-b-o@proton.me>
2024-01-23 02:46:07 -0300

    mais um exception

---
+++ src/main/java/br/com/companhia/aeroporto/exception/DataIntegrityViolationException.java
@@ -0,0 +1,17 @@
+package br.com.companhia.aeroporto.exception;
+
+import java.io.Serial;
+
+public class DataIntegrityViolationException extends RuntimeException {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public DataIntegrityViolationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DataIntegrityViolationException(String message) {
+        super(message);
+    }
+}
