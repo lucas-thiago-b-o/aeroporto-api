54e58f0850ca1ed445e4edb43544534c48926524
Lucas Thiago <lucas-thiago-b-o@proton.me>
2024-01-23 02:37:20 -0300

    mudando import para um só

--- src/main/java/br/com/companhia/aeroporto/controller/AeroportoResource.java
+++ src/main/java/br/com/companhia/aeroporto/controller/AeroportoResource.java
@@ -4,10 +4,7 @@ import br.com.companhia.aeroporto.dto.AeroportoDTO;
 import br.com.companhia.aeroporto.service.AeroportoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
