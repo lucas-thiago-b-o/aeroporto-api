c89ee47be84b61287f2b041e9b9b28a1dfff6330
Lucas Thiago <lucas-thiago-b-o@proton.me>
2024-01-23 21:40:02 -0300

    adição de resource e service para voo (Buscas por enquanto)

--- src/main/java/br/com/companhia/aeroporto/controller/VooResource.java
+++ src/main/java/br/com/companhia/aeroporto/controller/VooResource.java
@@ -1,10 +1,38 @@
 package br.com.companhia.aeroporto.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.companhia.aeroporto.dto.VooDTO;
+import br.com.companhia.aeroporto.service.VooService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping(value = "/voos")
+@RequestMapping(value = "/api/voos")
 public class VooResource {
 
+    @Autowired
+    private VooService vooService;
+
+    @GetMapping(value = "/voo/{id}")
+    public ResponseEntity<VooDTO> findById(@PathVariable Long id) {
+        return Optional.ofNullable(vooService.findById(id)).map(ResponseEntity::ok).orElse(ResponseEntity.noContent().build());
+    }
+
+    @GetMapping(value = "/voo/aeroporto/{id}")
+    public ResponseEntity<List<VooDTO>> findAllByAeroporoId(@PathVariable Long id) {
+        return Optional.ofNullable(vooService.findAllByAeroporoId(id)).map(ResponseEntity::ok).orElse(ResponseEntity.noContent().build());
+    }
+
+    @GetMapping(value = "/voo/aeroporto/cidade/{idCidade}")
+    public ResponseEntity<List<VooDTO>> findAllByCidadeId(@PathVariable Long idCidade) {
+        return Optional.ofNullable(vooService.findAllByCidadeId(idCidade)).map(ResponseEntity::ok).orElse(ResponseEntity.noContent().build());
+    }
+
+    @GetMapping(value = "/voo/aeroporto/estado/{idEstado}")
+    public ResponseEntity<List<VooDTO>> findAllByEstadoId(@PathVariable Long idEstado) {
+        return Optional.ofNullable(vooService.findAllByUfId(idEstado)).map(ResponseEntity::ok).orElse(ResponseEntity.noContent().build());
+    }
 }
--- src/main/java/br/com/companhia/aeroporto/service/VooService.java
+++ src/main/java/br/com/companhia/aeroporto/service/VooService.java
@@ -1,10 +1,40 @@
 package br.com.companhia.aeroporto.service;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.companhia.aeroporto.domain.Voo;
+import br.com.companhia.aeroporto.dto.VooDTO;
+import br.com.companhia.aeroporto.exception.ObjectNotFoundException;
+import br.com.companhia.aeroporto.model.ModelMapping;
+import br.com.companhia.aeroporto.repository.VooRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-@RestController
-@RequestMapping(value = "/voos")
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class VooService {
 
+    @Autowired
+    private VooRepository vooRepository;
+
+    @Autowired
+    private ModelMapping<Voo, VooDTO> modelMapping;
+
+    public List<VooDTO> findAllByCidadeId(Long id) {
+        return modelMapping.convertToDtoList(vooRepository.findAllCidadeId(id), VooDTO.class);
+    }
+
+    public List<VooDTO> findAllByUfId(Long id) {
+        return modelMapping.convertToDtoList(vooRepository.findAllByUfId(id), VooDTO.class);
+    }
+
+    public List<VooDTO> findAllByAeroporoId(Long id) {
+        return modelMapping.convertToDtoList(vooRepository.findAllByAeroportoOrigemId(id), VooDTO.class);
+    }
+
+    public VooDTO findById(Long id) {
+        return Optional.of(vooRepository.findById(id)).filter(Optional::isPresent)
+                .map(m -> modelMapping.convertToDto(m.get(), VooDTO.class))
+                .orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
 }
