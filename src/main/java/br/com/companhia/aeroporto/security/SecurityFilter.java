0c5af46e43f052a597ea152596be3833d2edbf9a
Lucas Thiago <lucas-thiago-b-o@proton.me>
2024-01-23 21:14:44 -0300

    Colocando verificação para identificar se valor existe

--- src/main/java/br/com/companhia/aeroporto/security/SecurityFilter.java
+++ src/main/java/br/com/companhia/aeroporto/security/SecurityFilter.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Objects;
 
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
@@ -31,8 +32,10 @@ public class SecurityFilter extends OncePerRequestFilter {
             var username = tokenService.validateToken(token);
             UserDetails userDetails = usuarioRepository.findByUsername(username);
 
-            var authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+            if (Objects.nonNull(userDetails)) {
+                var authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
         }
 
         filterChain.doFilter(request, response);
