9a6dd414b6211866b42b1e15ba5a236aa2243a43
Lucas Thiago <lucas-thiago-b-o@proton.me>
2024-01-28 17:51:59 -0300

    coodificação ClasseRepositoy

--- src/main/java/br/com/companhia/aeroporto/repository/ClasseRepository.java
+++ src/main/java/br/com/companhia/aeroporto/repository/ClasseRepository.java
@@ -6,9 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ClasseRepository extends JpaRepository<Classe, Long> {
 
     @Query(value = "UPDATE Classe c SET c.assento.passageiro = ?1 WHERE c.id = ?2 AND c.voo.id = ?3")
     void updateAssentoVooDaPassagemComprada(Passageiro passageiro, Long classeId, Long vooId);
+
+    List<Classe> findAllByVooId(Long id);
 }
