3585f95fa2545e501198d8088b31f03168340440
Lucas Thiago <lucas-thiago-b-o@proton.me>
2024-01-28 17:51:39 -0300

    inclusão de codificação nas classes da Classe

--- src/main/java/br/com/companhia/aeroporto/controller/ClasseResource.java
+++ src/main/java/br/com/companhia/aeroporto/controller/ClasseResource.java
@@ -1,10 +1,37 @@
 package br.com.companhia.aeroporto.controller;
 
+import br.com.companhia.aeroporto.dto.ClasseDTO;
+import br.com.companhia.aeroporto.service.ClasseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/api/classes")
 public class ClasseResource {
+
+    @Autowired
+    private ClasseService classeService;
+
+    @GetMapping(value = "/classe")
+    public ResponseEntity<List<ClasseDTO>> findAll() {
+        return Optional.ofNullable(classeService.findAll()).map(ResponseEntity::ok).orElse(ResponseEntity.noContent().build());
+    }
+
+    @GetMapping(value = "/classe/{id}")
+    public ResponseEntity<ClasseDTO> findById(@PathVariable Long id) {
+        return Optional.ofNullable(classeService.findById(id)).map(ResponseEntity::ok).orElse(ResponseEntity.noContent().build());
+    }
+
+    @GetMapping(value = "/classe/voo/{id}")
+    public ResponseEntity<List<ClasseDTO>> getAllClassesByVooId(@PathVariable Long id) {
+        return Optional.ofNullable(classeService.getAllClassesByVooId(id)).map(ResponseEntity::ok).orElse(ResponseEntity.noContent().build());
+    }
 
 }
--- src/main/java/br/com/companhia/aeroporto/service/ClasseService.java
+++ src/main/java/br/com/companhia/aeroporto/service/ClasseService.java
@@ -1,10 +1,37 @@
 package br.com.companhia.aeroporto.service;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.companhia.aeroporto.domain.Classe;
+import br.com.companhia.aeroporto.dto.ClasseDTO;
+import br.com.companhia.aeroporto.exception.ObjectNotFoundException;
+import br.com.companhia.aeroporto.model.ModelMapping;
+import br.com.companhia.aeroporto.repository.ClasseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-@RestController
-@RequestMapping(value = "/classes")
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ClasseService {
+
+    @Autowired
+    private ClasseRepository classeRepository;
+
+    @Autowired
+    private ModelMapping<Classe, ClasseDTO> modelMapping;
+
+    public List<ClasseDTO> findAll() {
+        return modelMapping.convertToDtoList(classeRepository.findAll(), ClasseDTO.class);
+    }
+
+    public List<ClasseDTO> getAllClassesByVooId(Long id) {
+        return modelMapping.convertToDtoList(classeRepository.findAllByVooId(id), ClasseDTO.class);
+    }
+
+    public ClasseDTO findById(Long id) {
+        return Optional.of(classeRepository.findById(id)).filter(Optional::isPresent)
+                .map(m -> modelMapping.convertToDto(m.get(), ClasseDTO.class))
+                .orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
 
 }
