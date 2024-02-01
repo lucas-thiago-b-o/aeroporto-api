98d52dd36b6ca9e6514ce07bf0ba9f822034acc3
Lucas Thiago <lucas-thiago-b-o@proton.me>
2024-02-01 20:44:03 -0300

    criação de consultas

--- src/main/java/br/com/companhia/aeroporto/repository/AssentoRepository.java
+++ src/main/java/br/com/companhia/aeroporto/repository/AssentoRepository.java
@@ -2,9 +2,12 @@ package br.com.companhia.aeroporto.repository;
 
 import br.com.companhia.aeroporto.domain.Assento;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AssentoRepository extends JpaRepository<Assento, Long> {
 
+    @Query(value = "SELECT c.assento FROM Classe c WHERE c.id = ?1")
+    Assento findAssentoByClasseId(Long idClasse);
 }
--- src/main/java/br/com/companhia/aeroporto/repository/BagagemRepository.java
+++ src/main/java/br/com/companhia/aeroporto/repository/BagagemRepository.java
@@ -2,9 +2,14 @@ package br.com.companhia.aeroporto.repository;
 
 import br.com.companhia.aeroporto.domain.Bagagem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BagagemRepository extends JpaRepository<Bagagem, Long> {
 
+    @Query(value = "SELECT p.bagagem FROM Passageiro p WHERE p.id = ?1")
+    List<Bagagem> findBagagensByPassageiroId(Long idPassageiro);
 }
